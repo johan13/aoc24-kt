56109ded14bfcf7557c95029828143f65f0ce703
Johan Levin <johan13@gmail.com>
2025-10-06 19:20:08 +0200

    Day 01

--- src/main/kotlin/aoc24/Day01.kt
+++ src/main/kotlin/aoc24/Day01.kt
@@ -1,5 +1,26 @@
 package aoc24
 
-fun day01p1(): Int = 2285373 // TODO
+import java.io.File
+import kotlin.math.abs
 
-fun day01p2(): Int = 21142653 // TODO
+fun day01p1(): Int {
+    val (left, right) = readInput()
+    return left
+        .sorted()
+        .zip(right.sorted())
+        .sumOf { (l, r) -> abs(l - r) }
+}
+
+fun day01p2(): Int {
+    val (left, right) = readInput()
+    val countMap = right.groupingBy { it }.eachCount()
+    return left.sumOf { it * (countMap[it] ?: 0) }
+}
+
+private val WHITESPACE = Regex("\\s+")
+
+private fun readInput(): Pair<List<Int>, List<Int>> =
+    File("inputs/input01.txt")
+        .readLines()
+        .map { it.split(WHITESPACE).let { (l, r) -> l.toInt() to r.toInt() } }
+        .unzip()
